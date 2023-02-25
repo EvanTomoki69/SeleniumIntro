d96edbd9aeab2a9dc74de150c019dc139822b5ea
EvanTomoki69 <106935385+EvanTomoki69@users.noreply.github.com>
2023-02-25 14:53:19 -0600

    Add files via upload

---
+++ selenium_automation/SeleniumCheckboxes.java
@@ -0,0 +1,51 @@
+package selenium_automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumCheckboxes 
+{
+	public static void main(String[] args) throws InterruptedException
+	{
+		Thread.sleep(1500);
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\ChromeDriver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.rahulshettyacademy.com/angularpractice/");
+		
+		WebElement nameField = driver.findElement(By.name("name"));
+		nameField.clear();
+		nameField.sendKeys("Evan Tomoki");
+		
+		WebElement mailField = driver.findElement(By.name("email"));
+		mailField.clear();
+		mailField.sendKeys("evan@gmail.com");
+		
+		WebElement passwordField = driver.findElement(By.id("exampleInputPassword1"));
+		passwordField.clear();
+		passwordField.sendKeys("evan69");
+		
+		WebElement checkBox = driver.findElement(By.id("exampleCheck1"));
+		checkBox.click();
+		
+		WebElement staticDropdown = driver.findElement(By.id("exampleFormControlSelect1"));
+		Select dropdown = new Select(staticDropdown);
+		dropdown.selectByIndex(1);
+		
+		WebElement checkCircle = driver.findElement(By.id("inlineRadio2"));
+		checkCircle.click();
+		
+		WebElement bdayField = driver.findElement(By.name("bday"));
+		bdayField.clear();
+		bdayField.sendKeys("04/04/1999");
+		
+		WebElement submit = driver.findElement(By.className("btn-success"));
+		submit.click();
+	}
+
+}
